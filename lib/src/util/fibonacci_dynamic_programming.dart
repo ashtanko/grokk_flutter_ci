43f749282df6ef0bec00f00b04edb645a00b0230
Oleksii Shtanko <shtankopro@gmail.com>
2022-04-13 22:39:25 +0300

    Small fixes

--- lib/src/util/fibonacci_dynamic_programming.dart
+++ lib/src/util/fibonacci_dynamic_programming.dart
@@ -4,7 +4,8 @@
 List<int> dp = List.generate((1e6 + 1).toInt(), (e) => -1);
 int mod = (1e9 + 7).toInt();
 
-//Get the nth Fibonacci number modulo 10^9 + 7 since it can be a very large number
+//Get the nth Fibonacci number modulo 10^9 + 7
+//since it can be a very large number
 int getFib(int n) {
   if (dp[n] == -1) dp[n] = (getFib(n - 1) % mod) + (getFib(n - 2) % mod);
   return dp[n] % mod;
